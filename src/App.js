ceab41e229158cb3957129dfa756aa7290e27275
Gentlefrank <apple@apples-MacBook-Pro.local>
2025-03-20 10:33:36 +0100

    style 80% completed

--- src/App.js
+++ src/App.js
@@ -1,39 +1,45 @@
 import logo from './logo.svg';
 import ColorPicker from '@rc-component/color-picker';
 import '@rc-component/color-picker/assets/index.css';
-
+import { useRef } from 'react';
 function App() {
+const bgColor= useRef(null)
+
+const changeBoxBg= ()=>{
+
+}
   return (
     <div className="App">
     <section className='banner'>
      <div className='container'>
       <div className='banner-box'>
-        jjnjj
+         
       </div>
      </div>
 
     </section>
     <section className='banner-items container'>
     <form>
-    <div className='form-group'>
+    <div className='banner-items-options'>
+    <div className='form-group form-bg-picker'>
     <ColorPicker />
-    <label>Pick background color</label>
+    <label>Pick a background color</label>
     
     </div>
-     <div className='form-group'>
-    <label>Upload an Image</label>
-    <input />
+     <div className='form-group form-img-upload'>
+    <label>Upload an Image:</label>
+    <input type='file' id='myFile' name='filename'/>
     </div>
-    <div className='form-group'>
-    <label>Heading</label>
+    <div className='form-group form-heading'>
+    <label>Heading:</label>
     <input />
     </div>
 
-    <div className='form-group'>
-    <label>Text</label>
+    <div className='form-group form-text'>
+    <label>Text:</label>
     <input />
     </div>
-   
+    </div>
     </form>
     </section>
     </div>
